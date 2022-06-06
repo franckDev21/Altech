23b8eac293a78f1d9ed21a453f509370d86e86af
mitridavinci <davincimitri@gmail.com>
2022-06-06 09:33:57 +0200

    about advanced

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'flaticon-books':"url('/src/assets/flaticon-books.png')",
+        'flaticon-24': "url('/src/assets/flaticon-24.png')",
+        'flaticon-coding':"url('/src/assets/flaticon-coding.png')",
+        'flaticon-reporter':"url('/src/assets/flaticon-reporter.jpg')",
+      }
+    },
   },
   plugins: [],
 }
